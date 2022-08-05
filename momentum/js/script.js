ee6d10cae32fe76f3475c1598122896d719ade8b
EXisTAnZ <ozim_ssspam@mail.ru>
2022-08-05 16:18:25 +0300

    added slideshow 	modified:   momentum/js/script.js

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,20 +1,29 @@
-const slidePrev = document.querySelector('.slide-prev'),
-    slideNext = document.querySelector('.slide-next');
+const body = document.querySelector('body'),
+  slidePrev = document.querySelector('.slide-prev'),
+  slideNext = document.querySelector('.slide-next');
 
 function getRandomSlide(max) {
-    return Math.floor(Math.random() * max) + 1;
+  return Math.floor(Math.random() * max) + 1;
 }
 
 const setBg = () => {
-    const timeOfDay = getTimeOfDay();
-    const bgNum = getRandomSlide(20);
-  
-    const img = new Image();
-    img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/tree/assets/images/${timeOfDay}/${bgNum.toString().padStart(2, '0')}.jpg`;
-    img.onload = () => {
-      body.style.backgroundImage = `url(${img.src})`;
-    };
+  const timeOfDay = getTimeOfDay();
+  const bgNum = getRandomSlide(20);
+
+  const img = new Image();
+  img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum.toString().padStart(2, '0')}.jpg`;
+  img.onload = () => {
+    body.style.backgroundImage = `url(${img.src})`;
   };
+};
 
 slideNext.addEventListener('click', setBg);
 slidePrev.addEventListener('click', setBg);
+
+const getTimeOfDay = () => {
+  const date = new Date();
+  const hours = date.getHours();
+  const arrTimesOfDay = ['night', 'morning', 'afternoon', 'evening'];
+  const id = Math.floor(hours / 6);
+  return arrTimesOfDay[id];
+};
