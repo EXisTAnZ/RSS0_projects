97b421c3ea94787381d1897adee42b8269689ce2
EXisTAnZ <ozim_ssspam@mail.ru>
2022-08-06 09:09:32 +0300

    added weather vidjet 	modified:   momentum/js/script.js

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -3,8 +3,14 @@ const body = document.querySelector('body'),
   slideNext = document.querySelector('.slide-next'),
   time = document.querySelector('.time'),
   date = document.querySelector('.date'),
-  greeting = document.queryCommandValue('.greeting'),
-  name = document.querySelector('.name')
+  greeting = document.querySelector('.greeting'),
+  name = document.querySelector('.name'),
+  city = document.querySelector('.city'),
+  weatherIcon = document.querySelector('.weather-icon'),
+  temperature = document.querySelector('.temperature'),
+  weatherDescription = document.querySelector('.weather-description'),
+  wind = document.querySelector('.wind'),
+  humidity = document.querySelector('.humidity')
   
 
 function getRandomSlide(max) {
@@ -57,18 +63,41 @@ showDate();
 
 const showGreeting = () => {
   const timeOfDay = getTimeOfDay();
-  greeting.textContent = `Good ${timeOfDay}`;
+  greeting.textContent = `Good ${timeOfDay},`;
 };
 showGreeting();
 
 function setLocalStorage() {
   localStorage.setItem('name', name.value);
+  localStorage.setItem('city', city.value);
 }
 window.addEventListener('beforeunload', setLocalStorage)
 
 function getLocalStorage() {
-  if(localStorage.getItem('name')) {
-    name.value = localStorage.getItem('name');
-  }
+  if(localStorage.getItem('name')) name.value = localStorage.getItem('name');
+  if (localStorage.getItem('city')) city.value = localStorage.getItem('city');
+  else city.value = 'Magas';
 }
+
 window.addEventListener('load', getLocalStorage)
+
+async function getWeather() {
+  city.value = localStorage.getItem('city');
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=6ce0dfa51ad31904fa3bf991156c8033&units=metric`;
+  const res = await fetch(url);
+  const data = await res.json();
+
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    temperature.textContent = `${Math.floor(data.main.temp)}°C`;
+    weatherDescription.textContent = data.weather[0].description;
+    wind.textContent = `Wind speed: ${Math.floor(data.wind.speed)} m/s`;
+    humidity.textContent = `Humidity: ${data.main.humidity}%`;
+
+}
+getWeather();
+
+city.onchange = () => {
+  localStorage.setItem('city', city.value);
+  weatherIcon.className = 'weather-icon owf';
+  getWeather();
+};
